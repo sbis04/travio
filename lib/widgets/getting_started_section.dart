ee16b27579e62279b2e2c0f67774b66f124bdccb
Souvik Biswas <sbis1999@gmail.com>
2025-08-30 12:53:40 +0530

    use some nice transformation

--- lib/widgets/getting_started_section.dart
+++ lib/widgets/getting_started_section.dart
@@ -31,7 +31,7 @@ class GettingStartedSection extends StatelessWidget {
               ),
               ConstrainedBox(
                 constraints: BoxConstraints(
-                  maxWidth: 860,
+                  maxWidth: max(580, context.appWidth * 0.4),
                 ),
                 child: SizedBox(width: double.infinity),
               ),
@@ -50,7 +50,7 @@ class GettingStartedSection extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: ConstrainedBox(
               constraints: BoxConstraints(
-                maxWidth: 860,
+                maxWidth: max(580, context.appWidth * 0.4),
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -138,46 +138,64 @@ class _HoverableImageState extends State<HoverableImage> {
 
   @override
   Widget build(BuildContext context) {
-    return Transform.translate(
-      offset: widget.translate,
-      child: Transform.scale(
-        scale: widget.scale,
-        child: Transform.rotate(
-          angle: widget.rotate, // Rotate slightly clockwise (towards right)
-          child: Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.identity()
-              ..setEntry(3, 2, 0.001) // perspective
-              ..rotateY(widget.tilt), // 3D tilt inward on the right
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.2),
-                    blurRadius: 18,
-                    offset: Offset(0, 2),
-                    spreadRadius: 2,
-                  ),
-                  BoxShadow(
-                    color: Colors.black.withValues(alpha: 0.1),
-                    blurRadius: 40,
-                    offset: Offset(0, 20),
-                    spreadRadius: 0,
-                  ),
-                ],
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(16),
-                child: AnimatedOpacity(
+    // Calculate hover offsets - small adjustments on hover
+    final hoverTranslateOffset = _isHovered
+        ? Offset(0, -10) // Just lift up slightly on hover
+        : Offset.zero;
+    final hoverRotateAngle = _isHovered
+        ? widget.rotate * 0.2 // Add slight additional rotation
+        : 0.0;
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: Transform.translate(
+        offset: widget.translate,
+        child: Transform.scale(
+          scale: widget.scale,
+          child: Transform.rotate(
+            angle: widget.rotate,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 400),
+              curve: Curves.easeOutBack,
+              transform: Matrix4.identity()
+                ..translate(hoverTranslateOffset.dx, hoverTranslateOffset.dy)
+                ..rotateZ(hoverRotateAngle),
+              child: Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..setEntry(3, 2, 0.001) // perspective
+                  ..rotateY(widget.tilt), // 3D tilt inward on the right
+                child: AnimatedContainer(
                   duration: const Duration(milliseconds: 300),
                   curve: Curves.easeInOut,
-                  opacity: _isHovered ? 1.0 : 0.7,
-                  child: MouseRegion(
-                    onEnter: (_) => setState(() => _isHovered = true),
-                    onExit: (_) => setState(() => _isHovered = false),
-                    child: Image.asset(
-                      widget.image,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black
+                            .withValues(alpha: _isHovered ? 0.3 : 0.2),
+                        blurRadius: _isHovered ? 25 : 18,
+                        offset: Offset(0, _isHovered ? 5 : 2),
+                        spreadRadius: 2,
+                      ),
+                      BoxShadow(
+                        color: Colors.black.withValues(alpha: 0.1),
+                        blurRadius: 40,
+                        offset: Offset(0, 20),
+                        spreadRadius: 0,
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: AnimatedOpacity(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      opacity: _isHovered ? 1.0 : 0.7,
+                      child: Image.asset(
+                        widget.image,
+                      ),
                     ),
                   ),
                 ),
